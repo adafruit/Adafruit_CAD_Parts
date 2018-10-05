c9e291b94043e2b1241502ac10801384a7c1d54e
Jesse Friedman <jesse@jesse.ws>
2018-10-05 00:28:12 -0400

    Adding -v option to get-previews.py

--- get-previews.py
+++ get-previews.py
@@ -6,6 +6,7 @@ from zipfile import ZipFile
 
 parser = ArgumentParser(description='Extract thumbnail images from F3D files')
 parser.add_argument('--force', action='store_true', help='overwrite existing thumbnails')
+parser.add_argument('-v', action='store_true', help='enable verbose logging')
 args = parser.parse_args()
 
 f3dfiles = glob('**/*.f3d')
@@ -13,9 +14,13 @@ f3dfiles = glob('**/*.f3d')
 for f3dfile in f3dfiles:
     productname = dirname(f3dfile)
     targetimagepath = join(productname, '{}.png'.format(productname))
-    if exists(targetimagepath) and not args.force:
-        print('Preview image for {} already exists; skipping (pass --force to override)'.format(productname))
-        continue
+    if exists(targetimagepath):
+        if args.force:
+            print('Preview image for {} already exists, but --force was specified so we\'re overwriting it'.format(productname))
+        else:
+            if args.v:
+                print('Preview image for {} already exists; skipping (pass --force to override)'.format(productname))
+            continue
 
     zipfile = ZipFile(f3dfile)
     pngs = fnfilter(zipfile.namelist(), '**/Previews/*.png')
